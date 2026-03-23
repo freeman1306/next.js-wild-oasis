2832ba01f4f6a582907286c92068b61c5ef2dc86
freeman1306 <zavalkin06@gmail.com>
2026-03-23 12:05:24 +0200

    starting with next.js

---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+function Page() {
+  return (
+    <div>Account Page</div>
+  )
+}
+
+export default Page
---
+++ app/cabins/page.js
@@ -0,0 +1,22 @@
+﻿import React from 'react'
+import Navigation from '../components/Navigation'
+
+async function Page() {
+
+   const res = await fetch('https://jsonplaceholder.typicode.com/users')
+   const data = await res.jspn()
+   console.log(data);
+   
+
+    return (
+      
+        <>
+         
+    <div>Cabins Page</div>
+
+    
+        </>
+  )
+}
+
+export default Page
